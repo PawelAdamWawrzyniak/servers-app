0ae154137ac8f6d0bf6c4d84ec0b60e5350c00a4
Paweł Wawrzyniak <pawel.adam.wawrzyniak@gmial.com>
2025-08-02 17:20:54 +0200

    Add Traits for the enums

--- app/Enums/ServerStatus.php
+++ app/Enums/ServerStatus.php
@@ -2,8 +2,27 @@
 
 namespace App\Enums;
 
-enum ServerStatus: string
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasIcon;
+
+enum ServerStatus: string implements HasColor, HasIcon
 {
     case ONLINE = 'online';
     case OFFLINE = 'offline';
+
+    public function getIcon(): string
+    {
+        return match ($this) {
+            self::ONLINE => 'heroicon-o-check-circle',
+            self::OFFLINE => 'heroicon-o-x-circle',
+        };
+    }
+
+    public function getColor(): string|array|null
+    {
+        return match ($this) {
+            self::ONLINE => 'success',
+            self::OFFLINE => 'danger',
+        };
+    }
 }
