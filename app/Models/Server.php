ca4fba64c4a4aa13a9785b8d9a500f3cb0f99984
Paweł Wawrzyniak <84261438+PawelAdamWawrzyniak@users.noreply.github.com>
2025-08-02 16:40:07 +0200

    Install laravel-ide-helper
    * Fix the register method
    
    * Install laravel-ide-helper
    
    * Generate docs
    
    ---------
    
    Co-authored-by: Paweł Wawrzyniak <pawel.adam.wawrzyniak@gmial.com>

--- app/Models/Server.php
+++ app/Models/Server.php
@@ -4,10 +4,34 @@ namespace App\Models;
 
 use App\Enums\ServerStatus;
 use Database\Factories\ServerFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property string $id
+ * @property string $name
+ * @property string $ip_address
+ * @property string $port
+ * @property ServerStatus|null $status
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static ServerFactory factory($count = null, $state = [])
+ * @method static Builder<static>|Server newModelQuery()
+ * @method static Builder<static>|Server newQuery()
+ * @method static Builder<static>|Server query()
+ * @method static Builder<static>|Server whereCreatedAt($value)
+ * @method static Builder<static>|Server whereId($value)
+ * @method static Builder<static>|Server whereIpAddress($value)
+ * @method static Builder<static>|Server whereName($value)
+ * @method static Builder<static>|Server wherePort($value)
+ * @method static Builder<static>|Server whereStatus($value)
+ * @method static Builder<static>|Server whereUpdatedAt($value)
+ * @mixin Eloquent
+ */
 class Server extends Model
 {
     /** @use HasFactory<ServerFactory> */
